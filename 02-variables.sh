f8faebe61e95ebb43800dec258b7e8c9c06d377b
saibabu663 <saibabu663@gmail.com>
2024-04-25 16:40:47 +0530

    hello script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: : hello suresh, how r u ?"
+echo "Suresh: : hi Ramesh, I am fine, how r u ?"
+echo "Ramesh: iam fine too.how is your work?"
+echo "suresh: : not bad. I am thinking to upgrade to Devops
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Ramesh #no space between = and value
+Person2=Suresh
+
+echo "$Ramesh: : hello $suresh, how r u ?"
+echo "$Suresh: : hi $Ramesh, I am fine, how r u ?"
+echo "$Ramesh: iam fine too.how is your work?"
+echo "$suresh: : not bad. I am thinking to upgrade to Devops
