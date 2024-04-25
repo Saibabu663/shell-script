46c9d3d14dfdd1f96d204f957d8c6550ab4ffbf1
saibabu663 <saibabu663@gmail.com>
2024-04-25 17:03:15 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter username: :"
+
+read USERNAME #there USERNAME is variabke
+
+
+echo "please enter password: :"
+
+read Password
+
+echo "username is: $username, password is:  $password"
