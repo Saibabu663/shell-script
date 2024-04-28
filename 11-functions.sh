54086e35301e0e591587b4aca50ddd19266623fb
saibabu663 <saibabu663@gmail.com>
2024-04-28 15:32:04 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,12 +5,12 @@ userid=$(id -u)
 
 validate(){
     if[ $1 -ne 0]
-    then
+    then 
         echo "$2.....failure"
         exit 1
     else
         echo "$2.....success"
-   
+    fi
 }
 
 if [ $userid -ne 0 ] 
