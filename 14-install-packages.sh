67e99e05d760208c4a8bb4605d21c58cb7cf0335
saibabu663 <saibabu663@gmail.com>
2024-04-29 16:26:42 +0530

    functions

---
+++ 14-install-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ] 
+then     
+    echo "please run this script with root access."
+    exit 1 # manually exit if error comes.
+    else
+        echo "you are super user."
+fi
+    
+  echo " all packages: $@"
