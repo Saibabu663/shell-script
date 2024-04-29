ebd202a85095749073f6c9ddbefbd6d52c29f565
saibabu663 <saibabu663@gmail.com>
2024-04-29 16:08:58 +0530

    functions

--- 12-colours.sh
+++ 12-colours.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 userid=$(id -u)
-TIMESTAMP=$(DATE +%f-%h-%m-%s)
+TIMESTAMP=$(date +%f-%h-%m-%s)
 script_name=$(echo $0 |cut -d "." -f1)                                                                                                                -f1
 
 logfile=/temp/$script_name-$timestamp.log
@@ -10,14 +10,14 @@ G="le[32m"
 N="le[0m"
 
 
-echo "script started ececuting at: $timestamp"
+echo "script started executing at: $timestamp"
 validate(){
    if [ $1 -ne 0]
    then
         echo -e "$2...$r failuru" $N"
         exit 1
     else
-        echo "$2...$G success" $N
+        echo -e "$2...$G success" $N
     fi
 }
 if [ $userid -ne 0 ] 
