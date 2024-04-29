336e4225e3cd7d73352dfcd62dfe60ed462b22cf
saibabu663 <saibabu663@gmail.com>
2024-04-29 16:46:44 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 userid=$(id -u)
+TIMESTAMP=$(date +%f-%h-%m-%s)
+script_name=$(echo $0 |cut -d "." -f1)                                                                                                            -f1
+logfile=/temp/$script_name-$timestamp.log
+
+validate(){
+   if [ $1 -ne 0]
+   then
+        echo "$2...failuru"
+        exit 1
+    else
+        echo "$2...success"
+    fi
+}
 
 if [ $userid -ne 0 ] 
 then     
@@ -13,4 +26,13 @@ fi
   for i in $@
   do 
     echo "package to install: $i"
+    dnf list installed $i &>>$logfile
+    if [$? -ne0 ]
+    then
+            echo "$i already installed...skipping"
+        else
+            echo "$i not installed...need to install"
+        fi
+
+
     done
